06851bbd5c7c6586a30057e5845925e74101421e
killychan <295630580@qq.com>
2019-03-02 18:15:01 +0800

    复习排序算法

---
+++ sort/intro_sort.py
@@ -0,0 +1,17 @@
+"""."""
+from comp_func import cmp_func
+
+'''
+关键点 ：
+'''
+
+
+def IntroSort(sort_nums, is_descend=False):
+    cmp_func(1, 2)
+
+# TestData = [23, 44, 56, 1, 2, 55, 61, 19, 20, 33, 44, 5, 56, 73, 92, 70]
+# print("before_sort: ", TestData)
+# IntroSort(TestData)
+# print("after_sort_increase:  ", TestData)
+# IntroSort(TestData, True)
+# print("after_sort_descend:  ", TestData)
